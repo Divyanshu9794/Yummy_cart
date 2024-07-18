641640596efaa52906e69a3dd39468d11a11625b
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-07-18 20:52:54 +0530

    .

--- app/src/main/java/com/example/yummycart/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/yummycart/Fragment/HomeFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.interfaces.ItemClickListener
 import com.denzcoskun.imageslider.models.SlideModel
 import com.example.yummycart.R
+import com.example.yummycart.adapter.PopularAdapter
 import com.example.yummycart.databinding.FragmentHomeBinding
 
 
@@ -41,7 +46,58 @@ class HomeFragment : Fragment() {
         imageSlider.setImageList(imageList)
 
         imageSlider.setImageList(imageList, ScaleTypes.FIT)
+
+        imageSlider.setItemClickListener(object :ItemClickListener{
+            override fun doubleClick(position: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(position: Int) {
+                val itemPosition = imageList[position]
+                val itemMessage = "Selected Image  $position"
+                Toast.makeText(requireContext(),itemMessage,Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        val foodname = listOf("Burger",
+            "Palak Paneer",
+            "Chole Bhature",
+            "Samosa",
+            "Aloo Gobi", "Paneer Tikka", "Masala Dosa",
+            "Dal Makhani", "Pani Puri", "Gulab Jamun", "Pav Bhaji", "Malai Kofta", "Bhindi Masala", "Matar Paneer", "Rajma",
+            "Baingan Bharta", "Vegetable Biryani", "Aloo Tikki", "Dhokla", "Kadhi Pakora", "Rasam","Idli", "Upma", "Poha", "Aloo Paratha",
+            "Dosa", "Pesarattu", "Vada", "Sabudana Khichdi", "Thepla", "Appam", "Misal Pav",
+            "Medu Vada", "Chole Kulche", "Puttu", "Moong Dal Cheela", "Besan Chilla", "Pongal", "Vegetable Sandwich",
+
+            "Bread Pakora")
+
+        val Price = listOf("₹150", "₹180", "₹130", "₹90", "₹120", "₹170", "₹110", "₹140", "₹60", "₹50",
+            "₹160", "₹190", "₹100", "₹80", "₹70", "₹110", "₹130", "₹50", "₹90", "₹80", "₹60", "₹70",
+            "₹110", "₹50", "₹140", "₹160", "₹130", "₹90", "₹100", "₹70", "₹150", "₹120", "₹60", "₹110",
+            "₹80", "₹90", "₹100", "₹150", "₹70", "₹60")
+
+        val popularfoodImages = listOf(R.drawable.menu1,R.drawable.menu2,R.drawable.menu3,R.drawable.menu4,
+            R.drawable.menu1,R.drawable.menu2,R.drawable.menu3,R.drawable.menu4,
+            R.drawable.menu1,R.drawable.menu2,R.drawable.menu3,R.drawable.menu4,
+            R.drawable.menu1,R.drawable.menu2,R.drawable.menu3,R.drawable.menu4,
+            R.drawable.menu1,R.drawable.menu2,R.drawable.menu3,R.drawable.menu4,
+            R.drawable.menu1,R.drawable.menu2,R.drawable.menu3,R.drawable.menu4,
+            R.drawable.menu1,R.drawable.menu2,R.drawable.menu3,R.drawable.menu4,
+            R.drawable.menu1,R.drawable.menu2,R.drawable.menu3,R.drawable.menu4,
+            R.drawable.menu1,R.drawable.menu2,R.drawable.menu3,R.drawable.menu4,
+            R.drawable.menu1,R.drawable.menu2,R.drawable.menu3,R.drawable.menu4)
+        val adapter = PopularAdapter(foodname,Price,popularfoodImages)
+        binding.popularRecyclerView.layoutManager =LinearLayoutManager(requireContext())
+        binding.popularRecyclerView.adapter = adapter
+
+
+
+
+
+
     }
+
+
 
     companion object {
 
