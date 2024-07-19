8934c71599ab74361fadd1055f85909378753bf9
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-07-20 00:05:45 +0530

    .

---
+++ app/src/main/java/com/example/yummycart/adapter/MenuAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.yummycart.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MenuAdapter(private val menuItems:MutableList<String>,private val menuItemprice: MutableList<String>, private val MenuImage:MutableList<Int>): RecyclerView.Adapter<MenuAdapter.MenuViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuViewHolder {
+        TODO("Not yet implemented")
+    }
+
+
+
+    override fun onBindViewHolder(holder: MenuViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+    override fun getItemCount(): Int =menuItems.size
+    inner class MenuViewHolder {
+
+    }
+}
