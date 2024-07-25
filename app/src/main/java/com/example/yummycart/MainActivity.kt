b005da16c4e9a8e2060efa5fdfc319d086be1ccf
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-07-25 20:41:54 +0530

    .

--- app/src/main/java/com/example/yummycart/MainActivity.kt
+++ app/src/main/java/com/example/yummycart/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.yummycart
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,16 +9,24 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.yummycart.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(LayoutInflater)
+        setContentView(binding.root)
         var NavController = findNavController(R.id.fragmentContainerView3)
         var bottomnav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomnav.setupWithNavController(NavController)
+        binding.notificationButton.setOnClickListener{
+            val bottomSheetDialog = NotificationBottomFragment()
+            bottomSheetDialog.show(supportFragmentManager,"Test")
+        }
 
     }
 }
