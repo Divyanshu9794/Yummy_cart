e42823f6e02777607f9b1b90d6df3d8d566ea0ac
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-07-26 11:33:26 +0530

    .

--- app/src/main/java/com/example/yummycart/adapter/MenuAdapter.kt
+++ app/src/main/java/com/example/yummycart/adapter/MenuAdapter.kt
@@ -1,13 +1,19 @@
 package com.example.yummycart.adapter
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View.OnClickListener
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.yummycart.DetailsActivity
 import com.example.yummycart.databinding.MenuItemBinding
 
-class MenuAdapter(private val menuItemsName: List<String>, private val menuItemprice: List<String>, private val MenuImage: List<Int>): RecyclerView.Adapter<MenuAdapter.MenuViewHolder>() {
+@Suppress("DEPRECATION")
+class MenuAdapter(private val menuItemsName: List<String>, private val menuItemprice: List<String>, private val MenuImage: List<Int>, private val requireContext:Context): RecyclerView.Adapter<MenuAdapter.MenuViewHolder>() {
 
 
+    private val itemClickListener:OnClickListener ?= null
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuViewHolder {
         val binding = MenuItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         return MenuViewHolder(binding)
@@ -20,15 +26,35 @@ class MenuAdapter(private val menuItemsName: List<String>, private val menuItemp
     }
     override fun getItemCount(): Int =menuItemsName.size
     inner class MenuViewHolder(private val binding: MenuItemBinding):RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.root.setOnClickListener{
+                val position = adapterPosition
+                if(position!=RecyclerView.NO_POSITION){
+                    itemClickListener.onItemClick(position)
+                }
+            }
+        }
         fun bind(position: Int) {
             binding.apply {
                 menufoodname.text = menuItemsName[position]
                 menuprice.text=menuItemprice[position]
                 menuImage.setImageResource(MenuImage[position])
+
+                //set on click listener to open food details
+
+                val intent = Intent( requireContext,DetailsActivity::class.java )
+                intent.putExtra("MenuItemName",menuItemsName.get(position))
+                intent.putExtra("MenuItemImage",MenuImage.get(position))
+                requireContext.startActivity(intent)
             }
 
         }
 
 
     }
-}    
+}
+
+private fun OnClickListener?.onItemClick(position: Int) {
+
+
+}
