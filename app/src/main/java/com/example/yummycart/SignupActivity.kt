eebfc50ad893c4303ef3dbe54be55f925715c087
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-08-05 23:05:30 +0530

    .

--- app/src/main/java/com/example/yummycart/SignupActivity.kt
+++ app/src/main/java/com/example/yummycart/SignupActivity.kt
@@ -3,6 +3,7 @@ package com.example.yummycart
 import android.content.ContentProviderClient
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +11,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.yummycart.databinding.ActivitySignupBinding
 import com.example.yummycart.databinding.ActivityStartBinding
+import com.example.yummycart.model.UserModel
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.Firebase
@@ -56,6 +58,9 @@ class SignupActivity : AppCompatActivity() {
             if(email.isBlank() || username.isBlank()||password.isBlank()){
                 Toast.makeText(this,"Please Fill all the Details",Toast.LENGTH_SHORT).show()
             }
+            else{
+                createAccount(email,password)
+            }
         }
 
         binding.alreadyhave.setOnClickListener{
@@ -65,5 +70,36 @@ class SignupActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    private fun createAccount(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener {
+            task->
+            if(task.isSuccessful)
+            {
+                Toast.makeText(this,"Account Created Successfully",Toast.LENGTH_SHORT).show()
+                saveUserData()
+                startActivity(Intent(this,LoginActivity::class.java))
+                finish()
+            }
+            else{
+                Toast.makeText(this,"Account Creation Failed",Toast.LENGTH_SHORT).show()
+                Log.d("Account","createAccount:Failure",task.exception)
+            }
+        }
+    }
+
+    private fun saveUserData() {
+        username = binding.username.text.toString()
+        password = binding.password.text.toString().trim()
+        email = binding.editTextTextEmailAddress.text.toString().trim()
+
+        val user = UserModel(username,email,password)
+
+        val userId = FirebaseAuth.getInstance().currentUser!!.uid
+
+
+        //save data to firebase data base
+        database.child("user").child(userId).setValue(user)
     }
 }
---
+++ app/src/main/java/com/example/yummycart/model/UserModel.kt
@@ -0,0 +1,7 @@
+package com.example.yummycart.model
+
+data class UserModel(
+    val name:String?=null,
+    val email:String?=null,
+    val password:String?=null
+)
