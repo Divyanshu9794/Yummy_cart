7f7e4e120fc9838ba2f0fb448f7ded7413126848
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-08-05 11:16:13 +0530

    /

--- app/src/main/java/com/example/yummycart/SignupActivity.kt
+++ app/src/main/java/com/example/yummycart/SignupActivity.kt
@@ -3,6 +3,7 @@ package com.example.yummycart
 import android.content.ContentProviderClient
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,9 +12,12 @@ import com.example.yummycart.databinding.ActivitySignupBinding
 import com.example.yummycart.databinding.ActivityStartBinding
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.database
 
 class SignupActivity : AppCompatActivity() {
 
@@ -21,6 +25,7 @@ class SignupActivity : AppCompatActivity() {
     private lateinit var email:String
     private lateinit var password:String
     private lateinit var username:String
+
 
     private lateinit var auth :FirebaseAuth
     private lateinit var database: DatabaseReference
@@ -33,16 +38,32 @@ class SignupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(binding.root)
+
+        //Initializing firebase database
+
+        auth = Firebase.auth
+
+        //initializing firebase auth
+        database = Firebase.database.reference
+
+        binding.createaccountbutton.setOnClickListener {
+            username = binding.username.text.toString()
+
+            email = binding.editTextTextEmailAddress.text.toString().trim()
+
+            password =binding.password.toString().trim()
+
+            if(email.isBlank() || username.isBlank()||password.isBlank()){
+                Toast.makeText(this,"Please Fill all the Details",Toast.LENGTH_SHORT).show()
+            }
+        }
+
         binding.alreadyhave.setOnClickListener{
             val intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
 
         }
-        binding.button4.setOnClickListener{
-            val intent = Intent(this,ChooseLoactionActivity::class.java)
-            startActivity(intent)
 
-        }
 
     }
 }
