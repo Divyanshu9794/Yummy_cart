8b20a6bfea244296234e851b331a68d4d65c0ec1
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-08-08 13:07:25 +0530

    .

--- app/src/main/java/com/example/yummycart/DetailsActivity.kt
+++ app/src/main/java/com/example/yummycart/DetailsActivity.kt
@@ -2,12 +2,16 @@ package com.example.yummycart
 
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.bumptech.glide.Glide
 import com.example.yummycart.databinding.ActivityDetailsBinding
+import com.example.yummycart.model.CartItems
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class DetailsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailsBinding
@@ -16,6 +20,8 @@ class DetailsActivity : AppCompatActivity() {
     private var foodDescription: String? = null
     private var foodIngredients: String? = null
     private var foodPrice: String? = null
+
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailsBinding.inflate(layoutInflater)
@@ -38,7 +44,28 @@ class DetailsActivity : AppCompatActivity() {
 
         binding.imageButton.setOnClickListener {
             finish()
+
         }
 
+        binding.AddItemButton.setOnClickListener {
+            addItemToCart()
+        }
+
+
+    }
+
+    private fun addItemToCart() {
+        val database = FirebaseDatabase.getInstance().reference
+        val userId = auth.currentUser?.uid?:""
+
+        val cartItem = CartItems(foodName.toString(),foodPrice.toString(),foodDescription.toString(),foodImage.toString(),1)
+
+
+
+        database.child("user").child(userId).child("CartItems").push().setValue(cartItem).addOnSuccessListener {
+            Toast.makeText(this,"Item Added Successfully to the cart",Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener{
+            Toast.makeText(this,"Item Not added to the cart",Toast.LENGTH_SHORT).show()
+        }
     }
 }
