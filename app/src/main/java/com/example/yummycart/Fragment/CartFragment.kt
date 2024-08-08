7f59be2acd63c1964a9e99bf38931bf75373483f
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-08-08 21:18:36 +0530

    .

--- app/src/main/java/com/example/yummycart/Fragment/CartFragment.kt
+++ app/src/main/java/com/example/yummycart/Fragment/CartFragment.kt
@@ -23,20 +23,19 @@ import com.google.firebase.database.ValueEventListener
 
 class CartFragment : Fragment() {
 
-    private lateinit var binding:FragmentCartBinding
+    private lateinit var binding: FragmentCartBinding
 
-    private lateinit var auth :FirebaseAuth
+
+    private lateinit var auth: FirebaseAuth
     private lateinit var database: FirebaseDatabase
-    private lateinit var foodNames:MutableList<String>
-    private lateinit var foodPrices:MutableList<String>
-    private lateinit var foodDescriptions:MutableList<String>
-    private lateinit var foodImagesUri :MutableList<String>
-    private lateinit var foodIngredients:MutableList<String>
-    private lateinit var quantity:MutableList<Int>
+    private lateinit var foodNames: MutableList<String>
+    private lateinit var foodPrices: MutableList<String>
+    private lateinit var foodDescriptions: MutableList<String>
+    private lateinit var foodImagesUri: MutableList<String>
+    private lateinit var foodIngredients: MutableList<String>
+    private lateinit var quantity: MutableList<Int>
     private lateinit var cartAdapter: CartAdapter
-    private lateinit var userId:String
-
-
+    private lateinit var userId: String
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,40 +48,108 @@ class CartFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentCartBinding.inflate(inflater,container,false)
+        binding = FragmentCartBinding.inflate(inflater, container, false)
 
         auth = FirebaseAuth.getInstance()
         retrieveCartItems()
 
 
-        binding.proceedbutton.setOnClickListener{
-            val intent = Intent(requireContext(),PayOutActivity::class.java)
+        binding.proceedbutton.setOnClickListener {
+
+            //get ordered item details for delivery
+
+            getOrderItmesDetails()
+            val intent = Intent(requireContext(), PayOutActivity::class.java)
             startActivity(intent)
         }
 
         return binding.root
     }
 
+    private fun getOrderItmesDetails() {
+        val orderIdReference: DatabaseReference =
+            database.reference.child("user").child(userId).child("CartItems")
+        val foodName = mutableListOf<String>()
+        val foodPrice = mutableListOf<String>()
+        val foodImage = mutableListOf<String>()
+        val foodDescription = mutableListOf<String>()
+        val foodIngredient = mutableListOf<String>()
+        //get items quantities
+        val foodQuantities = cartAdapter.getUpdatedItemsQuantities()
+        orderIdReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (foodSnapshot in snapshot.children) {
+                    //get cart items to respective list
+                    val orderItems = foodSnapshot.getValue(CartItems::class.java)
+                    //add items details in to the list
+
+                    orderItems?.foodName?.let { foodName.add(it) }
+                    orderItems?.foodPrice?.let { foodPrice.add(it) }
+                    orderItems?.foodDescription?.let { foodDescription.add(it) }
+                    orderItems?.foodImage?.let { foodImage.add(it) }
+                    orderItems?.foodIngredient?.let { foodIngredient.add(it) }
+                }
+
+                orderNow(
+                    foodName,
+                    foodPrice,
+                    foodDescription,
+                    foodImage,
+                    foodIngredient,
+                    foodQuantities
+                )
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(requireContext(), "Order Failed", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
+
+    private fun orderNow(
+        foodName: MutableList<String>,
+        foodPrice: MutableList<String>,
+        foodDescription: MutableList<String>,
+        foodImage: MutableList<String>,
+        foodIngredient: MutableList<String>,
+        foodQuantities: MutableList<Int>
+    ) {
+
+        if (isAdded && context != null) {
+            val intent = Intent(requireContext(), PayOutActivity::class.java)
+            intent.putExtra("FoodItemName", foodName as ArrayList<String>)
+            intent.putExtra("FoodItemPrice", foodPrice as ArrayList<String>)
+            intent.putExtra("FoodItemNImage", foodImage as ArrayList<String>)
+            intent.putExtra("FoodItemDescription", foodDescription as ArrayList<String>)
+            intent.putExtra("FoodItemIngredient", foodIngredient as ArrayList<String>)
+            intent.putExtra("FoodItemQuantities", foodQuantities as ArrayList<Int>)
+            startActivity(intent)
+        }
+
+    }
+
     private fun retrieveCartItems() {
         database = FirebaseDatabase.getInstance()
-        userId = auth.currentUser?.uid?:""
-        val foodReference:DatabaseReference = database.reference.child("user").child(userId).child("CartItems")
+        userId = auth.currentUser?.uid ?: ""
+        val foodReference: DatabaseReference =
+            database.reference.child("user").child(userId).child("CartItems")
         foodNames = mutableListOf()
         foodPrices = mutableListOf()
         foodDescriptions = mutableListOf()
         foodIngredients = mutableListOf()
         foodImagesUri = mutableListOf()
-        quantity= mutableListOf()
+        quantity = mutableListOf()
 
         //fetch data from the database
 
-        foodReference.addListenerForSingleValueEvent(object :ValueEventListener{
+        foodReference.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                for (foodSnapshot in snapshot.children){
-                    val cartItems =foodSnapshot.getValue(CartItems::class.java)
+                for (foodSnapshot in snapshot.children) {
+                    val cartItems = foodSnapshot.getValue(CartItems::class.java)
 
 
-                        //adding cart items details to the list
+                    //adding cart items details to the list
                     cartItems?.foodName?.let { foodNames.add(it) }
                     cartItems?.foodPrice?.let { foodPrices.add(it) }
                     cartItems?.foodDescription?.let { foodDescriptions.add(it) }
@@ -96,9 +163,18 @@ class CartFragment : Fragment() {
             }
 
             private fun setAdapter() {
-                val adapter = CartAdapter(requireContext(),foodNames,foodPrices,foodDescriptions,foodImagesUri,quantity,foodIngredients)
+                val adapter = CartAdapter(
+                    requireContext(),
+                    foodNames,
+                    foodPrices,
+                    foodDescriptions,
+                    foodImagesUri,
+                    quantity,
+                    foodIngredients
+                )
 
-                binding.cartrecyclerview.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,false)
+                binding.cartrecyclerview.layoutManager =
+                    LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
 
                 binding.cartrecyclerview.adapter = adapter
             }
--- app/src/main/java/com/example/yummycart/adapter/CartAdapter.kt
+++ app/src/main/java/com/example/yummycart/adapter/CartAdapter.kt
@@ -61,6 +61,16 @@ class CartAdapter(
     }
 
     override fun getItemCount(): Int = cartItem.size
+
+
+    //get updated quantities
+    fun getUpdatedItemsQuantities(): MutableList<Int> {
+        val itemQuantity = mutableListOf<Int>()
+        itemQuantity.addAll(cartQuantity)
+        return itemQuantity
+
+    }
+
     inner class CartViewHolder(private val binding: CartItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(position: Int) {
