4786a457237fdf59a3c09367e89675a73fb5283e
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-08-08 11:49:11 +0530

    .

--- app/src/main/java/com/example/yummycart/MenuBottomSheetFragment.kt
+++ app/src/main/java/com/example/yummycart/MenuBottomSheetFragment.kt
@@ -9,14 +9,18 @@ import com.example.yummycart.adapter.MenuAdapter
 import com.example.yummycart.databinding.FragmentMenuBottomSheetBinding
 import com.example.yummycart.model.MenuItem
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class MenuBottomSheetFragment : BottomSheetDialogFragment() {
-    private lateinit var binding : FragmentMenuBottomSheetBinding
+    private lateinit var binding: FragmentMenuBottomSheetBinding
 
     private lateinit var database: FirebaseDatabase
-    private lateinit var menuItem: MutableList<MenuItem>
+    private lateinit var menuItems: MutableList<MenuItem>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,19 +31,47 @@ class MenuBottomSheetFragment : BottomSheetDialogFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentMenuBottomSheetBinding.inflate(inflater,container,false)
-        binding.buttonback.setOnClickListener{
+        binding = FragmentMenuBottomSheetBinding.inflate(inflater, container, false)
+        binding.buttonback.setOnClickListener {
             dismiss()
         }
         // Inflate the layout for this fragment
-       
-        val adapter= MenuAdapter(ArrayList(menufoodname),ArrayList(menuprice),ArrayList(menuImage),requireContext())
-        binding.menuRecyclerView.layoutManager = LinearLayoutManager(requireContext())
-        binding.menuRecyclerView.adapter = adapter
+        retriveMenuItems()
+
+
         return binding.root
+    }
+
+    private fun retriveMenuItems() {
+        database = FirebaseDatabase.getInstance()
+        val foodRef: DatabaseReference = database.reference.child("menu")
+        menuItems = mutableListOf()
+
+        foodRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (foodSnapshot in snapshot.children) {
+                    val menuItem = foodSnapshot.getValue(MenuItem::class.java)
+                    menuItem?.let { menuItems.add(it) }
+                }
+
+                setAdapter()
+            }
+
+            private fun setAdapter() {
+                val adapter = MenuAdapter(menuItems, requireContext())
+                binding.menuRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+                binding.menuRecyclerView.adapter = adapter
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
     }
 
     companion object {
 
     }
+
 }
