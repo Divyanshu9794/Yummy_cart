f6b68fab52b36ede82f0e1a46f3f8715ddb942ee
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-08-08 12:27:04 +0530

    .

--- app/src/main/java/com/example/yummycart/DetailsActivity.kt
+++ app/src/main/java/com/example/yummycart/DetailsActivity.kt
@@ -1,24 +1,41 @@
 package com.example.yummycart
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 import com.example.yummycart.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityDetailsBinding
+    private lateinit var binding: ActivityDetailsBinding
+    private var foodName: String? = null
+    private var foodImage: String? = null
+    private var foodDescription: String? = null
+    private var foodIngredients: String? = null
+    private var foodPrice: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityDetailsBinding.inflate(layoutInflater)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val foodName = intent.getStringExtra("MenuItemName")
-        val foodImage = intent.getIntExtra("MenuItemImage",0)
+        foodName = intent.getStringExtra("MenuItemName")
+        foodDescription = intent.getStringExtra("MenuItemDescription")
+        foodIngredients = intent.getStringExtra("MenuItemIngredients")
+        foodPrice = intent.getStringExtra("MenuItemPrice")
+        foodImage = intent.getStringExtra("MenuItemImage")
 
-        binding.detailFoodname.text = foodName
-        binding.detailFoodImage.setImageResource(foodImage)
+        with(binding) {
+            detailFoodname.text = foodName
+            detaildescription.text = foodDescription
+            detailIngredients.text = foodIngredients
+
+            Glide.with(this@DetailsActivity).load(Uri.parse(foodImage)).into(detailFoodImage)
+
+        }
+
         binding.imageButton.setOnClickListener {
             finish()
         }
