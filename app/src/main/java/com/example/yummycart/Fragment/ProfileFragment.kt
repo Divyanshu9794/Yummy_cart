7aa433009355242e0ab07a00dc58922cbbfec040
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-08-09 12:19:11 +0530

    .

--- app/src/main/java/com/example/yummycart/Fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/yummycart/Fragment/ProfileFragment.kt
@@ -6,8 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.yummycart.R
+import com.example.yummycart.databinding.FragmentProfileBinding
+import com.example.yummycart.model.UserModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+    private val auth = FirebaseAuth.getInstance()
+    private val database =FirebaseDatabase.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +29,40 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding= FragmentProfileBinding.inflate(inflater,container,false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        setUserData()
+
+        return binding.root
+
+
+
+    }
+
+    private fun setUserData() {
+        val userID =auth.currentUser?.uid
+        if(userID!=null){
+            val userReference = database.getReference("user").child(userID)
+
+            userReference.addListenerForSingleValueEvent(object :ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if(snapshot.exists()){
+                        val userProfile = snapshot.getValue(UserModel::class.java)
+                        if(userProfile!=null){
+                            binding.name.setText(userProfile.name)
+                            binding.address.setText(userProfile.address)
+                            binding.email.setText(userProfile.email)
+                            binding.phone.setText(userProfile.phone)
+                        }
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+        }
     }
 
 
