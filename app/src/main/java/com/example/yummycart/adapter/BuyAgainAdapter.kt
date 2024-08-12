9105c29ab1e985fa1984197fcbb662f1e91aea95
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-08-12 15:45:39 +0530

    .

--- app/src/main/java/com/example/yummycart/adapter/BuyAgainAdapter.kt
+++ app/src/main/java/com/example/yummycart/adapter/BuyAgainAdapter.kt
@@ -1,33 +1,48 @@
 package com.example.yummycart.adapter
 
+import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.yummycart.databinding.BuyAgainItemBinding
 
-class BuyAgainAdapter(private val buyAgainFoodname: ArrayList<String>,private val buyAgainFoodprice:ArrayList<String>,
-                      private val buyAgainFoodimage:ArrayList<Int>):
+class BuyAgainAdapter(
+    private val buyAgainFoodname: MutableList<String>,
+    private val buyAgainFoodprice: MutableList<String>,
+    private val buyAgainFoodimage: MutableList<String>,
+    private var requireContext: Context
+) :
     RecyclerView.Adapter<BuyAgainAdapter.BuyagainViewHolder>() {
 
     override fun onBindViewHolder(holder: BuyagainViewHolder, position: Int) {
-        holder.bind(buyAgainFoodname[position],buyAgainFoodprice[position],buyAgainFoodimage[position])
+        holder.bind(
+            buyAgainFoodname[position],
+            buyAgainFoodprice[position],
+            buyAgainFoodimage[position]
+        )
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BuyagainViewHolder {
-        val binding = BuyAgainItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-        return  BuyagainViewHolder(binding)
+        val binding =
+            BuyAgainItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return BuyagainViewHolder(binding)
     }
 
-    override fun getItemCount(): Int =buyAgainFoodname.size
-    class BuyagainViewHolder(private val binding: BuyAgainItemBinding):RecyclerView.ViewHolder(binding.root) {
-        fun bind(foodName: String, foodPrice: String, foodImage: Int) {
+    override fun getItemCount(): Int = buyAgainFoodname.size
+    inner class BuyagainViewHolder(private val binding: BuyAgainItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(foodName: String, foodPrice: String, foodImage: String) {
 
-            binding.buyAgainFoodName.text =foodName
-            binding.buyAgainFoodPrice.text =foodPrice
-            binding.buyAgainFoodImage.setImageResource(foodImage)
+            binding.buyAgainFoodName.text = foodName
+            binding.buyAgainFoodPrice.text = foodPrice
+            val uriString = foodImage
+            val uri = Uri.parse(uriString)
+            Glide.with(requireContext).load(uri).into(binding.buyAgainFoodImage)
         }
 
     }
-
 
 
 }
