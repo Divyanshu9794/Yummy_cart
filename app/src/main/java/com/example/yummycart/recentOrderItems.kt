678e0df89fd60e039e5134110b4443d59c13b435
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-08-12 20:39:03 +0530

    .

--- app/src/main/java/com/example/yummycart/recentOrderItems.kt
+++ app/src/main/java/com/example/yummycart/recentOrderItems.kt
@@ -9,12 +9,8 @@ import androidx.core.view.WindowInsetsCompat
 class recentOrderItems : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_recent_order_items)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
